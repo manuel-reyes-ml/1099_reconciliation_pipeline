43ea5ed5ea2a39a14c5d76bc36e38fdb252452cc
Manuel Reyes <manuelreyesv410@gmail.com>
2025-12-09 11:36:30 -0500

    Python: Implement src/ python package initialization __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+#Turn this directory into a package by adding an __init__.py file
+#Dosctring for the package
+"""
+1099 Reconciliation Pipeline
+
+This package contains the core modules for:
+
+- Loading Relius and Matrix Excel exports
+- Cleaning and normalizing data
+- Matching transactions
+- Building 1099 correction files
+
+"""
+
+#Impot modules to be exposed at the package level
+__all__ = [
+    "config",
+    "load_data",
+    #"clean_relius",
+    #"clean__matrix",
+    #"match_transactions",
+    #"build_correction_file",
+]
